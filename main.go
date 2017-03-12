142ea6931ebebaeed535f95f96b3acdb160c8cd4
adesegun.adeyemo <aaadeyemo@gmail.com>
2017-03-12 15:30:56 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"flag"
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	var vpn = flag.String("vpn", "MUS123EJ", "Vehicle Plate Number")
+	flag.Parse()
+	resp, err := http.Get(fmt.Sprintf("%s%s", "http://www.lsmvaapvs.org/search.php?vpn=", strings.TrimSpace(*vpn)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		fmt.Println("Registration Number is Invalid...!")
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	html := string(body)
+	sanitized := strings.Replace(html, "<!--", "", 9)
+	sanitized = strings.Replace(sanitized, "-->", "", 9)
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(sanitized))
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc.Find(".form-group p").Each(func(i int, s *goquery.Selection) {
+		fmt.Printf("%s ", s.Text())
+		if i%2 != 0 {
+			fmt.Println()
+		}
+	})
+}
