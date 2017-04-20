9fcf30e5e978824d322105f011ec24a094adf91a
alexcrownus <alexcrownus@gmail.com>
2017-04-20 16:04:58 +0100

    refactored to using a cli lib

---
+++ cmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/alexcrownus/vreg"
+	"github.com/caarlos0/spin"
+	"github.com/urfave/cli"
+	"os"
+)
+
+var version = "master"
+
+func main() {
+	os.Setenv("HTTP_PROXY", "http://172.16.10.20:8080")
+	app := cli.NewApp()
+	app.Name = "vreg"
+	app.Version = version
+	app.Author = "Adesegun Adeyemo (alexcrownus@gmail.com)"
+	app.Usage = "Command line tool for http://www.lsmvaapvs.org/index.php for checking vehicle registration details, just for fun"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "vpn",
+			Value: "JJJ895AX2",
+			Usage: "Vehicle Plate Number",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		var spin = spin.New("\033[36m %s .\033[m")
+		spin.Start()
+		defer spin.Stop()
+		err := vreg.Query(c.String("vpn"))
+		if err != nil {
+			spin.Stop()
+			return cli.NewExitError(err.Error(), 1)
+		}
+		return nil
+	}
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
+}
--- vreg.go
+++ vreg.go
@@ -1,40 +1,40 @@
-package main
+package vreg
 
 import (
+	"errors"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"strings"
-
-	"flag"
-	"github.com/PuerkitoBio/goquery"
 )
 
-func main() {
-	var vpn = flag.String("vpn", "JJJ895AX", "Vehicle Plate Number")
-	flag.Parse()
-	resp, err := http.Get(fmt.Sprintf("%s%s", "http://www.lsmvaapvs.org/search.php?vpn=", strings.TrimSpace(*vpn)))
+func Query(vpn string) error {
+	resp, err := http.Get(fmt.Sprintf("%s%s", "http://www.lsmvaapvs.org/search.php?vpn=", strings.TrimSpace(vpn)))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
-		fmt.Println("Registration Number is Invalid...!")
-		return
+		return errors.New("Registration Number is Invalid...!")
 	}
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
 	html := string(body)
 	//sanitized := strings.Replace(html, "<!--", "", 9)
 	//sanitized = strings.Replace(sanitized, "-->", "", 9)
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	fmt.Println()
 	doc.Find(".form-group p").Each(func(i int, s *goquery.Selection) {
 		fmt.Printf("%s ", s.Text())
 		if i%2 != 0 {
 			fmt.Println()
 		}
 	})
+	return nil
 }
